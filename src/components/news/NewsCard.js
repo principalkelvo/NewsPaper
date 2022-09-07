6285c2e4840d278fb1b96a6e3f1a1ae7f284396d
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-09-08 00:01:40 +0300

    creates a card for each blog

--- src/components/news/NewsCard.js
+++ src/components/news/NewsCard.js
@@ -3,13 +3,32 @@ import NewsCardCSS from "./NewsCard.module.css";
 // import { Link } from "react-router-dom";
 
 function NewsCard() {
-    return(
-        <>
-    <div className={NewsCardCSS.fullCard}>
-    <div className="columns is-multiline"></div>
-    </div>
+  return (
+    <>
+      <div className={NewsCardCSS.fullCard}>
+        <div className="columns is-multiline">
+          <div className="column is-4">
+            <div className={NewsCardCSS.card}>
+              <figure className={NewsCardCSS.image}>
+                <img src="#" alt="ProductName" />
+               
+              </figure>
+              <figcaption>
+                    <h4>
+                        CEO Looper
+                    </h4>
+                    <p>NPC breaking news</p>
+                    <div>
+                    <p>2k views</p>
+                    <p>2 min ago</p>
+                    </div>
+                </figcaption>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-    )
+  );
 }
 
 export default NewsCard;
