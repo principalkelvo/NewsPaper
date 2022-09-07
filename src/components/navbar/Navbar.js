aaa3c4b5c664727fc2c7780794b18b64843b8ec9
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-09-07 10:44:09 +0300

    adds navbar items and logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,35 +1,40 @@
-import NavbarCSS from "./Navbar.module.css";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import NavbarCSS from "./Navbar.module.css";
+import SearchBar from "./SearchBar";
 
 function Navbar() {
   return (
     <>
-       <div className={NavbarCSS.navbar}>
-            {/* logo  */}
-            <div className={NavbarCSS.navbarItems + " " + NavbarCSS.logo}>
-                World
-            </div>
+      <nav className={NavbarCSS.navbar}>
+        {/* logo  */}
+        <div className={NavbarCSS.navbarItems + " " + NavbarCSS.logo}>
+          World
         </div>
-        <div className={NavbarCSS.navbarItems}>
-          <ul>
-            <li>
-              <NavLink to="/" className={NavbarCSS.linkStyles}>
-                News
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" className={NavbarCSS.linkStyles}>
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact" className={NavbarCSS.linkStyles}>
-                Contact
-              </NavLink>
-            </li>
-          </ul>
+        <div className={NavbarCSS.navbarSearch}>
+        <SearchBar />
         </div>
+      </nav>
+      <nav className={NavbarCSS.navbarItems}>
+        <ul> 
+          <li>
+            <NavLink to="/" className={NavbarCSS.linkStyles}>
+              News
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className={NavbarCSS.linkStyles}>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className={NavbarCSS.linkStyles}>
+              Contact
+            </NavLink>
+          </li>
+        <hr/>
+        </ul>
+      </nav>
     </>
   );
 }
