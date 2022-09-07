7710373654660b4281b0fdab57ecf3de9df6889d
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-09-07 09:34:15 +0300

    adds a serch field

--- src/components/navbar/SearchBar.js
+++ src/components/navbar/SearchBar.js
@@ -1,10 +1,23 @@
 import React from "react";
+import SearchBarCSS from "./SearchBar.module.css";
 
-function SearchBar(){
-    return(
-        <>
-        <form>
-            </form>
-        </>
-    )
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+function SearchBar() {
+  return (
+    <>
+      <form className={SearchBarCSS.search} >
+        <input
+          className={SearchBarCSS.input}
+          type="text"
+          placeholder="Search"
+        ></input>
+        <button className={SearchBarCSS.searchInput}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
+      </form>
+    </>
+  );
 }
+export default SearchBar;
