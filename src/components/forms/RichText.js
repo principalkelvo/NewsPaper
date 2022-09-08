2ae2afd17bb66aeee74a8c5ac833d6a3ccc501d7
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-09-08 16:25:48 +0300

    RichText uses a plugin from jodit

---
+++ src/components/forms/RichText.js
@@ -0,0 +1,15 @@
+import React, { useRef } from "react";
+import JoditEditor from "jodit-react";
+
+import RichTextCSS from "./RichText.module.css";
+
+function RichText(setValue) {
+  const editor = useRef(null);
+  return (
+    <div className={RichTextCSS.editor}>
+      <JoditEditor ref={editor} onChange={(content) => setValue(content)} />
+    </div>
+  );
+}
+
+export default RichText;
