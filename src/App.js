70324f21f8cc70033e2095c635f93ad644616a47
principalkelvo <principalkelvo@gmail.com>
2022-09-24 19:40:46 +0300

    adds categories component gets data form backend for each category

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Hero from "./components/home/Hero";
 import News from "./components/news/News";
 import NewsView from "./components/news/NewsView";
+import Categories from "./components/news/Category";
 import Form from "./components/forms/Form";
 
 import Navbar from "./components/navbar/Navbar";
@@ -11,6 +12,7 @@ import Navbar from "./components/navbar/Navbar";
 function App() {
   //states
   const [blogs, setBlogs] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [search, setSearch] = useState([]);
 
   //get all blogs
@@ -18,6 +20,14 @@ function App() {
     fetch("http://localhost:9292/blogs")
       .then((r) => r.json())
       .then((blogs) => setBlogs(blogs));
+      console.log('mount once');
+
+  }, []);
+  //get all categories
+  useEffect(() => {
+    fetch("http://localhost:9292/categories")
+      .then((r) => r.json())
+      .then((categories) => setCategories(categories));
   }, []);
 
   //add blog
@@ -25,11 +35,15 @@ function App() {
     setBlogs([...blogs, newBlog]);
   }
 
-  console.log('all blogs',blogs);
+  // console.log("all blogs", blogs);
 
   return (
     <div className="App">
-      <Navbar search={search} onSearchChange={setSearch} />
+      <Navbar
+        search={search}
+        onSearchChange={setSearch}
+        categories={categories}
+      />
       {/* <Form onAddBlog={handleAddBlog}/> */}
       <Routes>
         <Route
@@ -39,6 +53,13 @@ function App() {
         />
         <Route exact path="/buying" element={<News blogs={blogs} />} />
         <Route exact path="/blogs/:id" element={<NewsView blogs={blogs} />} />
+        <Route
+          exact
+          path="/categories/:category"
+          element={
+              <Categories/>
+          }
+        />
         <Route
           exact
           path="/addBlogs"
