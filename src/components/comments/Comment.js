c85cc93e893c3d1f5aa0a31cd1384d01a50dfddf
principalkelvo <principalkelvo@gmail.com>
2022-09-25 22:26:15 +0300

    add comment component is not same as the one on the design

---
+++ src/components/comments/Comment.js
@@ -0,0 +1,47 @@
+import CommentCSS from "./Comment.module.css";
+import React, { useState } from "react";
+// import CommentCard from "./CommentCard";
+import Input from "../forms/Input";
+import Comments from "../comments/Comments"
+
+function Comment() {
+  const [input, setInput] = useState({
+    comment: "",
+  });
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (input.comment != null && input.comment.trim() !== "") {
+      console.log("onSubmit: ", input.comment);
+    } else {
+      console.log("Error");
+    }
+  };
+  return (
+    // is not same as the one on the design because it imports styles from the input component
+    <>
+      <form onSubmit={handleOnSubmit} className={CommentCSS.form}>
+        <Input
+          id="comment"
+          type="text"
+          name="comment"
+          value={input.comment}
+          onChange={handleOnChange}
+          label="Add a Comment ..."
+        />
+        <div className={CommentCSS.buttons}>
+          <button>Cancel</button>
+          <button type="submit">Comment</button>
+        </div>
+      </form>
+
+      <div className={CommentCSS.form}>
+        <Comments/>
+      </div>
+    </>
+  );
+}
+export default Comment;
